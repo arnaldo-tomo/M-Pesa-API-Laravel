0439ffcef7f79c1d4627f8bede15ef27f6aa1bec
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-22 00:17:14 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,50 +26,37 @@
                     <h6 class="text-center">Digite o seu número de Telefone M-Pesa</h6>
                     <form action="{{ route('pagamento') }}" method="post">
                         @csrf
-                        <div class="row">
-
-                            <span class="input-group-text" id="addon-wrapping">+258</span>
-                            <input type="tel" class="form-control" placeholder="valor" value="1"
-                                aria-label="valor" aria-describedby="addon-wrapping" required>
-                            <br>
-                            <br>
-                            <input type="tel" class="form-control" placeholder="numeroTelefone" value="258846474687"
-                                aria-label="numeroTelefone" aria-describedby="addon-wrapping" required>
-                            <br>
-                            <br>
-
-                            <input type="tel" class="form-control" placeholder="referenciaConta" value="11114"
-                                aria-label="referenciaConta" aria-describedby="addon-wrapping" required>
-                            <br>
-                            <br>
-
-                            <input type="tel" class="form-control" value="T12344CC" placeholder="descricao"
-                                aria-label="descricao" aria-describedby="addon-wrapping" required>
+                        <div class="modal-body">
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">+258</span>
+                                <input type="tel" minlength="9" maxlength="9" name="numero" class="form-control"
+                                    placeholder="84xxxxxxxx" aria-label="Username" aria-describedby="addon-wrapping"
+                                    required>
+                            </div>
                         </div>
+                        <div class="d-grid gap-2 container mb-4">
+                            <button class="btn btn-primary" id="btnFetch" type="Submit">PAGAR COM M-PESA</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="d-grid gap-2 container mb-4">
-                    <button class="btn btn-primary" type="Submit">PAGAR COM M-PESA</button>
-                </div>
-                </form>
             </div>
         </div>
-    </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("#btnFetch").click(function() {
-                $(this).prop("disabled", true);
-                $(this).html(
-                    `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Processando Opagamento...`
-                );
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $("#btnFetch").click(function() {
+                    $(this).prop("disabled", true);
+                    $(this).html(
+                        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Processando Opagamento...`
+                    );
+                });
             });
-        });
-    </script>
+        </script>
 
 </body>
 
